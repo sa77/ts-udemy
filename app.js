ac17316a7c1ba6cff358d836ec609fde3457162e
Sandeep Acharya <acharya877@gmail.com>
2019-12-20 10:54:11 +1100

    26 - functions as types

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result - ', num);
 }
+// this is pretty standard use case
+// this is not quite useful - but if you have any use-case that requires this - then go ahead - but understand the difference
+// NOTE - :void is inferred by default return type if you're not returning anything from a function
 function printResult2(num) {
     console.log('Result - ', num);
     return;
@@ -14,3 +17,14 @@ printResult(add(5, 12));
 // undefied is a valid type in TS - but you cannot have :undefined as a return type from a function without a return statement
 // if you want `func(): undefined` to work - you'll need to have a return; statement at the end of that function
 var someValue;
+// store functions to variables by casting it to `Function`
+var combinedvalues = add;
+// NOTE ^^ this can take any function - does not care about the params or return value - `add` or `printResult` both are valid
+// to be more explicit - use arrow function - on the right side add the return type
+var combinedvalues2;
+//  this means combinedValues2 should accept any function that takes 2 parameter where each parameter is a number
+// and the return type of the function is also a number
+combinedvalues2 = add;
+// combinedvalues2 = printResult;
+// ^^ this is a mismatch
+console.log(combinedvalues(8, 8));
--- app.ts
+++ app.ts
@@ -21,3 +21,18 @@ printResult(add(5, 12));
 // undefied is a valid type in TS - but you cannot have :undefined as a return type from a function without a return statement
 // if you want `func(): undefined` to work - you'll need to have a return; statement at the end of that function
 let someValue: undefined;
+
+
+// store functions to variables by casting it to `Function`
+let combinedvalues: Function = add;
+// NOTE ^^ this can take any function - does not care about the params or return value - `add` or `printResult` both are valid
+
+// to be more explicit - use arrow function - on the right side add the return type
+let combinedvalues2: (a: number, b: number) => number;
+//  this means combinedValues2 should accept any function that takes 2 parameter where each parameter is a number
+// and the return type of the function is also a number
+combinedvalues2 = add;
+// combinedvalues2 = printResult;
+// ^^ this is a mismatch
+
+console.log(combinedvalues(8, 8))
