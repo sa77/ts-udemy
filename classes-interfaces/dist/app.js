deca700550fa0a95689ac91b6671adbc5c0bd0ee
Sandeep Acharya <acharya877@gmail.com>
2019-12-21 18:33:48 +1100

    60 - constructor and this keyword

--- classes-interfaces/dist/app.js
+++ classes-interfaces/dist/app.js
@@ -4,10 +4,20 @@ var Department = /** @class */ (function () {
     function Department(n) {
         this.name = n;
     }
+    // instance method is added like this - name of the method, no colon, no equal sign, with a paranthesis
+    Department.prototype.describe = function () {
+        console.log('Department - ' + this.name);
+    };
     return Department;
 }());
 // object instantiation
 // constructor is a function which is called with new keyword (has no return statement inside it, but it returns a new object)
 var accounting = new Department('Account');
 console.log(accounting);
+accounting.describe();
+// this works now because the object on RHS looks like a Department - JSON
+var accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+accountingCopy.describe();
+// `this` keyword - refers to the thing which is responsible for calling the method
+// to specify always use `this: ClassName` - so TS checks that the instance method is always called on an instance of that class not on some random thing
 //# sourceMappingURL=app.js.map
--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -5,11 +5,28 @@ class Department {
     constructor(n: string) {
         this.name = n;
     }
+
+    // instance method is added like this - name of the method, no colon, no equal sign, with a paranthesis
+    describe(this: Department) {
+        console.log('Department - ' + this.name)
+    }
 }
 
 // object instantiation
 // constructor is a function which is called with new keyword (has no return statement inside it, but it returns a new object)
 const accounting = new Department('Account');
 console.log(accounting)
+
+accounting.describe();
+
+// this works now because the object on RHS looks like a Department - JSON
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+accountingCopy.describe()
+
+
+// `this` keyword - refers to the thing which is responsible for calling the method
+// to specify always use `this: ClassName` - so TS checks that the instance method is always called on an instance of that class not on some random thing
+
 
 
