876616b664bb92c5510b4d47dcbc9df657b07b04
Sandeep Acharya <acharya877@gmail.com>
2019-12-21 18:44:05 +1100

    61 - private and public modifiers

--- classes-interfaces/dist/app.js
+++ classes-interfaces/dist/app.js
@@ -1,12 +1,21 @@
 "use strict";
 var Department = /** @class */ (function () {
+    // private is called a modifier. there's `public` as well - which is default if nothing is specified
     // this is syntactic sugar - can also be defined as function constructor() - es5
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     // instance method is added like this - name of the method, no colon, no equal sign, with a paranthesis
     Department.prototype.describe = function () {
         console.log('Department - ' + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
     };
     return Department;
 }());
@@ -16,8 +25,13 @@ var accounting = new Department('Account');
 console.log(accounting);
 accounting.describe();
 // this works now because the object on RHS looks like a Department - JSON
-var accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe()
 // `this` keyword - refers to the thing which is responsible for calling the method
 // to specify always use `this: ClassName` - so TS checks that the instance method is always called on an instance of that class not on some random thing
+accounting.addEmployee('Max');
+accounting.addEmployee('Sandy');
+// careful with this - make an uniform method to do it ==> private (both propery as well as method can be private)
+// accounting.employees[2] = 'Anna';
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,5 +1,7 @@
 class Department {
     name: string;
+    private employees: string[] = [];
+    // private is called a modifier. there's `public` as well - which is default if nothing is specified
 
     // this is syntactic sugar - can also be defined as function constructor() - es5
     constructor(n: string) {
@@ -8,7 +10,16 @@ class Department {
 
     // instance method is added like this - name of the method, no colon, no equal sign, with a paranthesis
     describe(this: Department) {
-        console.log('Department - ' + this.name)
+        console.log('Department - ' + this.name);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
@@ -20,9 +31,9 @@ console.log(accounting)
 accounting.describe();
 
 // this works now because the object on RHS looks like a Department - JSON
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
-accountingCopy.describe()
+// accountingCopy.describe()
 
 
 // `this` keyword - refers to the thing which is responsible for calling the method
@@ -30,3 +41,9 @@ accountingCopy.describe()
 
 
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Sandy');
+// careful with this - make an uniform method to do it ==> private (both propery as well as method can be private)
+// accounting.employees[2] = 'Anna';
+
+accounting.printEmployeeInformation()
