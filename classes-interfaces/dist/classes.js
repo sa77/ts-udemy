338068a1a2657dec7c2bd9cab74cbbb8f687b8b4
Sandeep Acharya <acharya877@gmail.com>
2019-12-23 12:03:27 +1100

    71 - interfaces

---
+++ classes-interfaces/dist/classes.js
@@ -0,0 +1,127 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Department = (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    Department.createEmployee = function (name) {
+        return { name: name };
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    Department.fiscalYear = 2020;
+    return Department;
+}());
+var employee = Department.createEmployee('Max');
+console.log('static --', employee);
+var ITDepartment = (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, 'IT-dept') || this;
+        _this.admins = admins;
+        _this.admins = admins;
+        return _this;
+    }
+    ITDepartment.prototype.describe = function () {
+        console.log('IT department - ID: ', this.id);
+    };
+    return ITDepartment;
+}(Department));
+var it = new ITDepartment('d2', ['Max']);
+console.log(it);
+var AccountDepartment = (function (_super) {
+    __extends(AccountDepartment, _super);
+    function AccountDepartment(id, reports) {
+        var _this = _super.call(this, id, 'Account-dept') || this;
+        _this.reports = reports;
+        _this.lastReport = reports[0];
+        return _this;
+    }
+    Object.defineProperty(AccountDepartment.prototype, "mostRecentReport", {
+        get: function () {
+            if (this.lastReport) {
+                return this.lastReport;
+            }
+            throw new Error('No report found.');
+        },
+        set: function (value) {
+            if (!value) {
+                throw new Error('Please pass in a valid value!');
+            }
+            this.addReport(value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AccountDepartment.prototype.describe = function () {
+        console.log('Accounting department - ID: ', this.id);
+    };
+    AccountDepartment.prototype.addEmployee = function (name) {
+        if (name == 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    };
+    AccountDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    };
+    AccountDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountDepartment;
+}(Department));
+var account2 = new AccountDepartment('d3', []);
+account2.addReport('Monthly expenses');
+account2.addReport('Yearly expenses');
+console.log(account2);
+console.log(account2.mostRecentReport);
+account2.describe();
+account2.addEmployee('Max');
+account2.addEmployee('Manu');
+account2.printReports();
+account2.printEmployeeInformation();
+account2.mostRecentReport = 'Weekly Expense';
+console.log(account2.mostRecentReport);
+var MarketingDepartment = (function (_super) {
+    __extends(MarketingDepartment, _super);
+    function MarketingDepartment(id, reports) {
+        var _this = _super.call(this, id, 'Marketing-dept') || this;
+        _this.reports = reports;
+        _this.lastReport = reports[0];
+        return _this;
+    }
+    MarketingDepartment.getInstance = function () {
+        if (MarketingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new MarketingDepartment('d3', []);
+        return this.instance;
+    };
+    MarketingDepartment.prototype.describe = function () {
+        console.log('Accounting department - ID: ', this.id);
+    };
+    return MarketingDepartment;
+}(Department));
+var marketing = MarketingDepartment.getInstance();
+console.log(marketing);
+//# sourceMappingURL=classes.js.map
---
+++ classes-interfaces/src/classes.ts
@@ -0,0 +1,192 @@
+// `abstract` classes cannot be instantiated themselves
+abstract class Department {
+    // private id:string;
+    // name: string;
+    protected employees: string[] = [];
+    // private is called a modifier. there's `public` as well - which is default if nothing is specified
+
+    // this is syntactic sugar - can also be defined as function constructor() - es5
+    // get rid of field definitions by using access modifier prefixed fields as params in constructor
+    // `readonly` property - cannot be changed once initialized
+    constructor(protected readonly id: string, public name: string) {
+        // this.id;
+        // this.name = name;
+    }
+
+    // -- static property
+    static fiscalYear = 2020;
+
+    // -- static method
+    // static members are detached from the instance and are part of the class instead
+    // access it outside the class - Department.createEmployee()
+    // these methods cannot be accessed from non-static parts inside the class using `this` keyword
+    static createEmployee(name: string) {
+        return {name: name};
+    }
+
+    // instance method is added like this - name of the method, no colon, no equal sign, with a paranthesis
+    abstract describe(this: Department): void;
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+// object instantiation
+// constructor is a function which is called with new keyword (has no return statement inside it, but it returns a new object)
+// const accounting = new Department('d1', 'Account');
+// console.log(accounting)
+
+// accounting.describe();
+
+// this works now because the object on RHS looks like a Department - JSON
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe()
+
+
+// `this` keyword - refers to the thing which is responsible for calling the method
+// to specify always use `this: ClassName` - so TS checks that the instance method is always called on an instance of that class not on some random thing
+
+
+
+// accounting.addEmployee('Max');
+// accounting.addEmployee('Sandy');
+// careful with this - make an uniform method to do it ==> private (both propery as well as method can be private)
+// accounting.employees[2] = 'Anna';
+
+// accounting.printEmployeeInformation()
+
+
+const employee = Department.createEmployee('Max');
+console.log('static --', employee)
+
+
+// 64 - class inheritence
+class ITDepartment extends Department {
+
+    constructor(id: string, public admins: string[]) {
+        // super calls the constructor of the parent class
+        super(id, 'IT-dept');
+        this.admins = admins;
+    }
+
+    describe(this: ITDepartment) {
+        console.log('IT department - ID: ', this.id)
+    }
+}
+
+const it = new ITDepartment('d2', ['Max']);
+console.log(it);
+
+
+class AccountDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.');
+    }
+
+    describe(this: AccountDepartment) {
+        console.log('Accounting department - ID: ', this.id)
+    }
+
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
+
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Account-dept');
+        this.lastReport = reports[0];
+    }
+
+    // this method appends to this.employees property (which is private property in Department class)
+    // enter `protected` modifier - when set - the property is accessible in inherited classes (`private` properties are not accessible from child)
+    addEmployee(name: string) {
+        if (name == 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+
+    printReports(this: AccountDepartment) {
+        console.log(this.reports)
+    }
+}
+
+
+const account2 = new AccountDepartment('d3', [])
+
+// account2.mostRecentReport = '';
+
+account2.addReport('Monthly expenses');
+account2.addReport('Yearly expenses');
+console.log(account2);
+console.log(account2.mostRecentReport)
+
+account2.describe()
+
+account2.addEmployee('Max');
+account2.addEmployee('Manu');
+
+account2.printReports();
+account2.printEmployeeInformation();
+
+account2.mostRecentReport = 'Weekly Expense';
+
+// you don't access it as a method - just access it as a property (behind the scene - it executes the getter)
+console.log(account2.mostRecentReport)
+
+
+
+// 69 --- singletons
+// singleton pattern - a pattern in OOP ==> pattern that ensures that you have only one instance of a certain class
+// => private constructor - this ensures that you can't call `new` to initiatiate multiple times
+
+class MarketingDepartment extends Department {
+    private lastReport: string;
+    private static instance: MarketingDepartment;
+
+    private constructor(id: string, private reports: string[]) {
+        super(id, 'Marketing-dept');
+        this.lastReport = reports[0];
+    }
+
+    static getInstance() {
+        // `this` keyword inside static method refers to the class itself - MarketingDepartment
+        // so `this.instance === MarketingDepartment.instance` inside a static method
+        if (MarketingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new MarketingDepartment('d3', []);
+        return this.instance;
+    }
+
+    describe(this: MarketingDepartment) {
+        console.log('Accounting department - ID: ', this.id)
+    }
+}
+
+const marketing = MarketingDepartment.getInstance();
+console.log(marketing);
+
+
+
+
+
