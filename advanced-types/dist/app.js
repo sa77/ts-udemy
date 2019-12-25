6705499c0f33fdaacdf2eae38dd02db59b54a0f7
Sandeep Acharya <acharya877@gmail.com>
2019-12-25 12:38:25 +1100

    83 - TS type guards

--- advanced-types/dist/app.js
+++ advanced-types/dist/app.js
@@ -8,3 +8,53 @@ var e1 = {
     startDate: new Date()
 };
 ;
+function add(a, b) {
+    // TG1 - this is a type-guard (using typeof) - although there's a flexibility of using Combinable, type-guard
+    // ensures that it runs correctly at runtime
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name -' + emp.name);
+    // TG2 - we can't use typeof here because we can't call typeof on custom types - JS recognize only built-in types
+    // we can use: 'property' in object
+    if ('privilages' in emp) {
+        console.log('Privilages - ' + emp.privilages);
+    }
+    if ('startDate' in emp) {
+        console.log('start date - ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+// TG3 - while working with classes - `instanceof`
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('Driving ... ');
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('Driving truck ...');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log('loading cargo .. ' + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    // instanceof is a built-in operator built in JS - not a new TS feature
+    // JS does not understand custom types (as required by `typeof`) but can understand constructors (used by `instanceof`)
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -44,3 +44,91 @@ type Numeric = number | boolean;
 
 // in this case Universal: number is resolved because `Combinable` and `Numeric` has only `number` as common
 type Universal = Combinable & Numeric;
+
+
+
+// === 83 - type guards
+// - used more often - helps with union types - helps to know which exact type you are getting at runtime
+// helps to work with union types but guard with runtime fail safety
+type CombinableTG = string | number;
+type NumericTG = number | boolean;
+
+//
+type UniversalTG = CombinableTG & NumericTG;
+
+function add(a: Combinable, b: Combinable) {
+    // TG1 - this is a type-guard (using typeof) - although there's a flexibility of using Combinable, type-guard
+    // ensures that it runs correctly at runtime
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name -' + emp.name);
+    // TG2 - we can't use typeof here because we can't call typeof on custom types - JS recognize only built-in types
+    // we can use: 'property' in object
+    if ('privilages' in emp) {
+        console.log('Privilages - ' + emp.privilages);
+    }
+    if ('startDate' in emp) {
+        console.log('start date - ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1);
+
+// TG3 - while working with classes - `instanceof`
+class Car {
+    drive() {
+        console.log('Driving ... ')
+    }
+}
+
+
+class Truck {
+    drive() {
+        console.log('Driving truck ...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('loading cargo .. ' + amount)
+    }
+}
+
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive()
+    // instanceof is a built-in operator built in JS - not a new TS feature
+    // JS does not understand custom types (as required by `typeof`) but can understand constructors (used by `instanceof`)
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
