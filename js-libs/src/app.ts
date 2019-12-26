1c7dbc0bb55c6c0a41f8f3f601b153dfc31d1d49
Sandeep Acharya <acharya877@gmail.com>
2019-12-26 13:57:08 +1100

    161 - using declare as last resort for global variables

--- js-libs/src/app.ts
+++ js-libs/src/app.ts
@@ -1,3 +1,8 @@
 import  _ from 'lodash';
 
+// `declare` allows us to let TS know of some global variables that TS doesn't inherently
+declare var GLOBAL: any;
+
 console.log(_.shuffle([1, 3, 9, 2]))
+
+console.log(GLOBAL);
