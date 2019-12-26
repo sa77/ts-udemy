7ba5c06922d5f8bd551daaf523b40e08efe06334
Sandeep Acharya <acharya877@gmail.com>
2019-12-26 12:55:22 +1100

    156 - webpack config for production

---
+++ webpack-basic/webpack.config.prod.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'none',
+  module: { // these rules are applied on a per-file basis
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [ // are applied to the general workflow
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
+};
+
+
+// clean-webpack-plugn => helps to cleanup dist folder whenever the project is rebuilt
