09c9416abb56061f8550acc536ff8d0bbc2675da
Sandeep Acharya <acharya877@gmail.com>
2019-12-26 14:14:52 +1100

    162 - using TS module class-transformer

--- js-libs/src/app.ts
+++ js-libs/src/app.ts
@@ -6,3 +6,42 @@ declare var GLOBAL: any;
 console.log(_.shuffle([1, 3, 9, 2]))
 
 console.log(GLOBAL);
+
+
+import { Product } from './product.model';
+
+
+const p1 = new Product('A book', 12.99);
+
+console.log(p1.getInformation());
+
+
+// if we get this from the server - this does not have a class used in TS (the `Product` type imported above)
+const products = [
+    {title: 'A carpet', price: 21.99},
+    {title: 'A pen', price: 10.99}
+];
+
+
+// one way to do it will be - manually transfrom these into an instance of specific constructor function
+const loadedProducts = products.map(prod => {
+    return new Product(prod.title, prod.price);
+})
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation())
+}
+
+
+// another way is to use class-transformer (TS - module [3rd party])
+
+// TS - module - https://github.com/typestack/class-transformer
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+
+const loadedProductsTransformed = plainToClass(Product, products);
+
+console.log('TS - class-transformer');
+for (const prod of loadedProductsTransformed) {
+    console.log(prod.getInformation())
+}
