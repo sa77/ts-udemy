60ea67b49da9d5866a8edd5de49143ee29ea6983
Sandeep Acharya <acharya877@gmail.com>
2019-12-27 19:06:41 +1100

    191 - add express middleware with TS syntax

--- ts-node/src/app.ts
+++ ts-node/src/app.ts
@@ -3,8 +3,20 @@
 // const express = require('express');
 
 // use this TS syntax instead (this is experimental in nodeJS writtern in commonJS)
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import todoRoutes from './routes/todos';
 
 const app = express();
+
+
+app.use('/todos', todoRoutes);
+
+// define a error handling expressJS middleware
+// this gets fired by express if any other middleware prior to this one has error
+// `next` is the next middleware in line for the rquest to continue
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
