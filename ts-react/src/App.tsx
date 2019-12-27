83d82e7a93a24687d822d019533ff913e35608af
Sandeep Acharya <acharya877@gmail.com>
2019-12-27 17:12:49 +1100

    181 - managing react states in TS

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
+import { Todo } from './todo.model';
+
+
+// `useState` is another hook - always returns an erray with 2 elements
+//     - first is the latest state snapshot for this render cycle
+//     - second is the function to update that state and re-render that component
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'Finish the course'}];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
     console.log(text);
+    // this also works (not ideal though)
+    // setTodos([...todos, {id: Math.random().toString(), text: text}]);
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id: Math.random().toString(), text: text}
+    ]);
   };
 
   return (
