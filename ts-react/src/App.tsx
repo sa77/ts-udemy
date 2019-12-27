178ad02220d5cae9632aadac2ac5f49c1cc818f2
Sandeep Acharya <acharya877@gmail.com>
2019-12-27 16:59:06 +1100

    179 - cross component communication with TS

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -5,9 +5,14 @@ import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{id: 't1', text: 'Finish the course'}];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
--- ts-react/src/components/NewTodo.tsx
+++ ts-react/src/components/NewTodo.tsx
@@ -1,13 +1,19 @@
 import React, { useRef } from 'react';
 
+// user interface/type (interface makes more sense) to setup type definition for a props
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+};
+
+
 // `useRef` is a hook
-const NewTodo: React.FC = () => {
+const NewTodo: React.FC<NewTodoProps> = (props) => {
     const textInputRef = useRef<HTMLInputElement>(null);
 
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault();
         const enteredText = textInputRef.current!.value;
-        console.log(enteredText);
+        props.onAddTodo(enteredText);
     };
 
     return (
